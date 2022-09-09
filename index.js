4cd13e8b0e7b1fc7aa335cecdd860c93d096c871
Adefemi <adefemi6k@gmail.com>
2022-09-09 11:44:21 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// // let num = 0;
+
+// // let output = 0; 
+
+// const ast = ['*','**', '***', '****', '*****']
+//     // for (let i=0; i <= 10; i++) {
+//     //     if(i > num) {
+//     //         output = i
+//     //         console.log(output+ (output -1));
+//     //     }
+//     // }
+ 
+//     for (let i=5; i >0 ; i--) {
+//         if(i < ast[i]) {
+//             console.log(i);
+//         }
+//     }
+ 
+let array = ['*****', '****', '***', '**', '*']
+
+for(let i = 0; i <= array.length; i++){
+    console.log(array[i]);
+}
+
